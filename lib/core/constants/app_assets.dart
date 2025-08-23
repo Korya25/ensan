d61330811be102fef6f4d66fcdf2877c8847826c
youssef hamada <yosefhagag84171@gmail.com>
2025-08-23 20:00:35 +0300

    edit logo obording

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,4 +1,6 @@
 class AppAssets {
-  static const String appLogoSlogenName =
+  static const String appLogoSlogenName1 =
       'assets/images/app_logo_slogen_name.png';
+  static const String appLogoSlogenName =
+      'assets/images/app_logo_slogen_name_copy.png';
 }
--- lib/features/onborading/widgets/app_logo_slogen_name_widget.dart
+++ lib/features/onborading/widgets/app_logo_slogen_name_widget.dart
@@ -6,6 +6,54 @@ class AppLogoSlogenNameWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(AppAssets.appLogoSlogenName);
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        // اللوجو
+        Image.asset(AppAssets.appLogoSlogenName, height: 120),
+
+        const SizedBox(height: 8),
+
+        // إنسان
+        Text(
+          'إنسان',
+          style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+            fontWeight: FontWeight.bold,
+          
+          ),
+          textAlign: TextAlign.center,
+        ),
+
+        // ensan
+        Text(
+          'ensan',
+          style: Theme.of(
+            context,
+          ).textTheme.headlineLarge?.copyWith(fontWeight: FontWeight.w500),
+          textAlign: TextAlign.center,
+        ),
+
+        const SizedBox(height: 6),
+
+        // بني جيل .. يبني حياة
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'يبني حياة .. ',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            Text(
+              'نبني جيل  ',
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                color: Colors.green,
+                fontFamily: 'Aria0',
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
